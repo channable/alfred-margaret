326469387ae7a9636030afccb31acdde4b206057
Ruud van Asseldonk <ruud@channable.com>
2019-03-06 11:38:37 +0100

    Add header to test suite

--- tests/AhoCorasickSpec.hs
+++ tests/AhoCorasickSpec.hs
@@ -1,3 +1,9 @@
+-- Alfred-Margaret: Fast Aho-Corasick string searching
+-- Copyright 2019 Channable
+--
+-- Licensed under the 3-clause BSD license, see the LICENSE file in the
+-- repository root.
+
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 
