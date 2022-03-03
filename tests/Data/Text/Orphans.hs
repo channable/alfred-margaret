651c0f70332d9eb992accd98f6ad044259973f9e
Paul Brinkmeier <paul.brinkmeier@channable.com>
2022-03-03 15:19:38 +0100

    Add tests for UTF-8 Replacer module

--- tests/Data/Text/Orphans.hs
+++ tests/Data/Text/Orphans.hs
@@ -4,7 +4,13 @@ import Test.QuickCheck (Arbitrary (..))
 
 import qualified Test.QuickCheck.Gen as Gen
 
-import Data.Text.AhoCorasick.Automaton (CaseSensitivity (..))
+import Data.Text.Utf8 as Utf8
+
+import Data.Text.CaseSensitivity (CaseSensitivity (..))
 
 instance Arbitrary CaseSensitivity where
   arbitrary = Gen.elements [CaseSensitive, IgnoreCase]
+
+-- TODO: Slow placeholder implementation until we can use text-2.0
+instance Arbitrary Utf8.Text where
+  arbitrary = fmap Utf8.pack arbitrary
