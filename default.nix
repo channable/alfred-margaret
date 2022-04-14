bfff9eeb9354a921e45d9f24fa370b99f6395f81
Paul Brinkmeier <paul.brinkmeier@channable.com>
2022-04-14 17:36:29 +0200

    Add benchmark tools to nix config

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 {
   pkgs ? import ./nix/nixpkgs-pinned.nix {},
-  hsTools ? false
+  # Use haskell-languager-server?
+  hsTools ? false,
+  # Use tools for the benchmarks in other languages (Cargo, Bazel, etc.)?
+  benchTools ? false
 }:
 let
   haskellDependencies = import ./nix/haskell-dependencies.nix;
@@ -26,7 +29,23 @@ let
     lib.optionals hsTools (with haskell.packages.ghc902; [
       haskell-language-server
       implicit-hie
-    ])
+    ]) ++
+    lib.optionals benchTools [
+      (python3.withPackages (pyPkgs: [
+        pyPkgs.clize
+        pyPkgs.numpy
+      ]))
+
+      # For rust implementation
+      cargo
+
+      # For java implementation (uses outdated bazel stuff)
+      jdk8
+      bazel_1
+
+      # For Haskell implementation
+      gmp
+    ]
   );
 in
   pkgs.buildEnv {
